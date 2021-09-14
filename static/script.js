886b629e68901dab1aa0df25109dfbadb94c86a7
Renan Gibeli <renan.gibeli@hotmail.com>
2021-09-14 11:22:07 -0300

    seção sobre a habilidades

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+//ABRE E FECHA MENU LATERAL EM MODO MOBILE
+
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
@@ -6,4 +8,17 @@ menuMobile.addEventListener('click', () => {
     ? menuMobile.classList.replace("bi-list", "bi-x")
     : menuMobile.classList.replace("bi-x", "bi-list")
     body.classList.toggle("menu-nav-active")
+})
+
+//FECHA O MENU QUANDO CLICAR EM ALGUM ITEM E MUDA O ICONE PARA LIST
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        if(body.classList.contains('menu-nav-active')){
+            body.classList.remove('menu-nav-active')
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
 })
